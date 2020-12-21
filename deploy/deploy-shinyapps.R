92b1c5900a805c3f76efb00b91fac91231ef4039
diwashrestha <diwash712@gmail.com>
2020-12-21 19:29:22 +0545

    Added screts

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -1,12 +1,12 @@
 # deploy/deploy-shinyapps.R
 # usethis::use_build_ignore("deploy")
 rsconnect::setAccountInfo(
-  Sys.getenv("SHINYAPPS_ACCOUNT"),
-  Sys.getenv("SHINYAPPS_TOKEN"),
-  Sys.getenv("SHINYAPPS_SECRET")
+  Sys.getenv("diwashrestha"),
+  Sys.getenv("E6341425D3EF2166DDB53E7A122AC379"),
+  Sys.getenv("YGJz1M6NMysYvkFMM6qRLKl+XXdUMX6Mm5ePelOC")
 )
 rsconnect::deployApp(
-  appName = "ShinyCICD",
+  appName = "datatable_trial",
   # exclude hidden files and renv directory (if present)
   appFiles = setdiff(list.files(), "renv")
 )
